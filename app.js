5d679dd9ae014d3c0580aaaf2b87f448db423143
susu725 <zxcv1010017@gmail.com>
2023-08-18 21:35:11 +0800

    feat: initialize api server

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const usePassport = require('./config/passport')
 // const getUser = authHelpers.getUser
 const { getUser } = require('./helpers/auth-helpers')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
-const routes = require('./routes')
+const { pages } = require('./routes')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
   res.locals.user = getUser(req)
   next()
 })
-app.use(routes)
+app.use(pages)
 
 app.listen(port, () => {
   console.info(`Example app listening on port ${port}!`)
